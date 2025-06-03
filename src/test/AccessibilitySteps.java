625067d1da52b6dc6702cab12ad7a3bb378027b6
jackpaul24 <157022463+jackpaul24@users.noreply.github.com>
2025-06-03 22:11:01 +0100

    Update AccessibilitySteps.java

--- src/test/AccessibilitySteps.java
+++ src/test/AccessibilitySteps.java
@@ -1,6 +1,9 @@
 package steps;
 
 import com.deque.axe.AXE;
+import net.serenitybdd.core.annotations.findby.By;
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.thucydides.core.annotations.Step;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -11,21 +14,14 @@ import java.net.URL;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class AccessibilitySteps {
+public class AccessibilitySteps extends UIInteractionSteps {
 
-    private static final URL scriptUrl = AccessibilitySteps.class.getResource("/axe.min.js");
-
-    WebDriver driver;
-
-    public AccessibilitySteps(WebDriver driver) {
-        this.driver = driver;
-    }
+    private static final URL scriptUrl = AccessibilitySteps.class.getClassLoader().getResource("axe.min.js");
 
     @Step("Check accessibility violations on the current page")
     public void checkAccessibility() {
-        JSONObject responseJSON = new AXE.Builder(driver, scriptUrl).analyze();
+        JSONObject responseJSON = new AXE.Builder(getDriver(), scriptUrl).analyze();
         JSONArray violations = responseJSON.getJSONArray("violations");
-
         if (violations.length() == 0) {
             System.out.println("No accessibility violations found.");
         } else {
